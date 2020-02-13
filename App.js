eb825563b1930d61afcd63316cf7c0c86bbc24c3
ShaabanReactNative <61009559+ShaabanReactNative@users.noreply.github.com>
2020-02-13 13:23:31 +0200

    Add files

---
+++ App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+import Root from './Common/Root';
+
+
+export default class App extends Component {
+  render() {
+    return (
+        <Root/>
+       
+    )
+  }
+}
